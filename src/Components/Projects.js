4ac86bbf2dad85af7d2257e1fdc1607f0f61b0d3
cyberkillua <moshood988@gmail.com>
2021-01-27 13:09:54 +0100

    new project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-
-
 const Projectss = styled.section`
   margin-top: 80px;
   width: 100%;
@@ -112,67 +110,81 @@ const Projectss = styled.section`
 const Projects = () => {
   return (
     <>
-    <Projectss id="projects">
-      <h3>Checkout some of my works</h3>
-      <div className="ProjectGrid">
-        <a href="https://instaclone70.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-          <div className="box">
-            <h4>Instagram Clone</h4>
-            <h6>React + Nodejs + MongoDB</h6>
-            <p>A web clone of Instagram</p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-        <a href="https://budgetappp.netlify.app/" target="_blank" rel="noopener noreferrer">
-        <div className="box">
-            <h4>Budget Calculator</h4>
-            <h6>HTML + CSS + Javascript</h6>
-            <p> Income vs expense</p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-        <a href="https://killuaquiz.netlify.app/" target="_blank" rel="noopener noreferrer">
-        <div className="box">
-            <h4>Quizer</h4>
-            <h6>React</h6>
-            <p>An Online Quiz App  </p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-        <a href="https://dicedd.netlify.app/" target="_blank" rel="noopener noreferrer">
-        <div className="box">
-            <h4>Pig Game</h4>
-            <h6>HTML + CSS + Javascript</h6>
-            <p>You vs Com, 2 dice who wins?</p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-        <a href="https://photowalled.netlify.app/" target="_blank" rel="noopener noreferrer">
-        <div className="box">
-            <h4>Photowall</h4>
-            <h6>React + Firebase</h6>
-            <p>An Online Album</p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-        <a href="https://relaxed-shirley-f10c3b.netlify.app/" target="_blank" rel="noopener noreferrer">
-        <div className="box">
-            <h4>Whoisng</h4>
-            <h6>React</h6>
-            <p>A webapp that searches and displays information about domains</p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-        <a href="https://getcryptonews.netlify.app/" target="_blank" rel="noopener noreferrer">
-        <div className="box">
-            <h4>Crypto News</h4>
-            <h6>Vuejs</h6>
-            <p>A webapp that displays news about Crypto currencies</p>
-            <div className="go">GO</div>
-          </div>
-        </a>
-      </div>
-    </Projectss>
+      <Projectss id="projects">
+        <h3>Checkout some of my works</h3>
+
+        <div className="ProjectGrid">
+          <a
+            href="https://getcryptonews.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className="box">
+              <h4>Crypto News</h4>
+              <h6>Vuejs</h6>
+              <p>A webapp that displays news about Crypto currencies</p>
+              <div className="go">GO</div>
+            </div>
+          </a>
+          <a
+            href="https://instaclone70.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className="box">
+              <h4>Instagram Clone</h4>
+              <h6>React + Nodejs + MongoDB</h6>
+              <p>A web clone of Instagram</p>
+              <div className="go">GO</div>
+            </div>
+          </a>
+          <a
+            href="https://budgetappp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className="box">
+              <h4>Budget Calculator</h4>
+              <h6>HTML + CSS + Javascript</h6>
+              <p> Income vs expense</p>
+              <div className="go">GO</div>
+            </div>
+          </a>
+          <a
+            href="https://killuaquiz.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className="box">
+              <h4>Quizer</h4>
+              <h6>React</h6>
+              <p>An Online Quiz App </p>
+              <div className="go">GO</div>
+            </div>
+          </a>
+          <a
+            href="https://dicedd.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className="box">
+              <h4>Pig Game</h4>
+              <h6>HTML + CSS + Javascript</h6>
+              <p>You vs Com, 2 dice who wins?</p>
+              <div className="go">GO</div>
+            </div>
+          </a>
+
+          <a
+            href="https://relaxed-shirley-f10c3b.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className="box">
+              <h4>Whoisng</h4>
+              <h6>React</h6>
+              <p>
+                A webapp that searches and displays information about domains
+              </p>
+              <div className="go">GO</div>
+            </div>
+          </a>
+        </div>
+      </Projectss>
     </>
   );
 };
